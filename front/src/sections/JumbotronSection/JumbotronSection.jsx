6a84300fd8ad325a1763e6dc6bc1243f512a447c
magancarz <grzmateusz6@gmail.com>
2023-01-18 18:14:17 +0100

    Revert "Revert "Revert "FIX: sections now work properly with server database"""
    This reverts commit 15a8de60aac0419930b6507e8645bfa65def585b.

--- front/src/sections/JumbotronSection/JumbotronSection.jsx
+++ front/src/sections/JumbotronSection/JumbotronSection.jsx
@@ -4,7 +4,6 @@ import BaseContainer from '../../components/atoms/BaseContainer/BaseContainer'
 import BaseFont from '../../components/atoms/BaseFont/BaseFont'
 import BaseButton from '../../components/molecules/BaseButton/BaseButton'
 const JumbotronSection = ({value}) => {
-	console.log(value)
 	return (
 		<section className={s.jumbotron_section}>
 			<BaseContainer>
@@ -12,16 +11,16 @@ const JumbotronSection = ({value}) => {
 					<div className={s.content_wrapper}>
 						<div className={s.content_wrapper_text_section}>
 							<BaseFont tag={'h1'}>
-								{value.value.header}
+								{value.header}
 							</BaseFont>
 							<BaseFont tag={'p'}>
-								{value.value.description}
+								{value.description}
 							</BaseFont>
 							<BaseButton theme={'secondary'} className={s.jumbotron_section_button}
 								text="Show more"
 							/>
 						</div>
-						<img src={value.images[0].image} alt={value.images[0].alt} title={value.images[0].title} />
+						<img src={value.image.image} alt={value.image.alt} title={value.image.title} />
 					</div>
 				</div>
 			</BaseContainer>
