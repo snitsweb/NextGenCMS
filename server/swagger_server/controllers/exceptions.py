0817f610b2225efd6ccf499762035d8955911806
mteplicki <261726@student.pwr.edu.pl>
2023-01-18 20:41:00 +0100

    * added triggers to prevent adding new subpage with the same path * also, app can handle this error * updated README

--- server/swagger_server/controllers/exceptions.py
+++ server/swagger_server/controllers/exceptions.py
@@ -5,13 +5,26 @@ class ExceptionHandler:
     class NotFoundException(RuntimeError):
         """Not found."""
 
+    class WrongInputException(RuntimeError):
+        """Wrong input."""
+        def __init__(self, message):
+            super().__init__(message)
+
     def not_found_handler(error):
         return {
             "detail": str(error),
             "status": 404,
             "title": "Not found"
         }, 404
+    
+    def wrong_input_handler(error):
+        return {
+            "detail": str(error),
+            "status": 400,
+            "title": "Wrong input"
+        }, 400
 
 def add_excpetion_handler(app : FlaskApp):
     app.add_error_handler(ExceptionHandler.NotFoundException, ExceptionHandler.not_found_handler)
+    app.add_error_handler(ExceptionHandler.WrongInputException, ExceptionHandler.wrong_input_handler)
     
